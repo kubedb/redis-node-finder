9684b21847e91653d365e8bf3232506d8e49771e
Shaad7 <abdullah.alshaad@appscode.com>
2022-03-11 16:55:57 +0600

    finding dns name using master and replica count

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/klog/v2"
@@ -11,15 +13,15 @@ import (
 )
 
 var (
-	//err        error
+	err error
 	//hostName   string
 	kubeConfig *restclient.Config
 	//kubeClient *kubernetes.Clientset
-	dbClient *cs.Clientset
+	dbClient     *cs.Clientset
+	offShootName string
 )
 
 func init() {
-	var err error
 	kubeConfig, err = restclient.InClusterConfig()
 	if err != nil {
 		klog.Fatalln(err)
@@ -32,17 +34,83 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Hello")
-	findRedisNodes()
+	fmt.Println("Finding redis nodes")
+	printDNSOfAllRedisNodes()
 }
 
-func findRedisNodes() {
-	nameSpace := "demo"
-	db, err := dbClient.KubedbV1alpha2().Redises(nameSpace).Get(context.TODO(), "dbname", metav1.GetOptions{})
+func writeInfoToFile(filename string, count int) {
+
+	filePath := fmt.Sprintf("/tmp/%s", filename)
+	file, err := os.Create(filePath)
 	if err != nil {
 		klog.Fatalln(err)
 		return
 	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			klog.Fatalln(err)
+		}
+	}(file)
+	_, err = file.WriteString(fmt.Sprintf("%d\n", count))
+	if err != nil {
+		klog.Fatalln(err)
+	}
+
+}
+
+func writePodDNSToFile(filename string, dnsNames []string) {
+
+	filePath := fmt.Sprintf("/tmp/%s", filename)
+	file, err := os.Create(filePath)
+	if err != nil {
+		klog.Fatalln(err)
+		return
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			klog.Fatalln(err)
+		}
+	}(file)
+	for _, dns := range dnsNames {
+		_, err = file.WriteString(dns + "\n")
+		if err != nil {
+			klog.Fatalln(err)
+			return
+		}
+	}
+}
+
+// We get db object and check how many masters and replicas are there
+// Then we generate dns names of the pods using pod name and governing service name
+// Then all the infos are stored in files in /tmp folder so that we can access from bash script
+func printDNSOfAllRedisNodes() {
+	nameSpace := os.Getenv("NAMESPACE")
+	offShootName = os.Getenv("OFF_SHOOT_NAME")
+	dbGoverningServiceName := os.Getenv("REDIS_GOVERNING_SERVICE")
+
+	db, err := dbClient.KubedbV1alpha2().Redises(nameSpace).Get(context.TODO(), offShootName, metav1.GetOptions{})
+	if err != nil {
+		klog.Fatalln(err)
+		return
+	}
+	dbMasterCount := int(*db.Spec.Cluster.Master)
 	dbReplicaCount := int(*db.Spec.Cluster.Replicas)
-	fmt.Println(dbReplicaCount)
+
+	writeInfoToFile("master.txt", dbMasterCount)
+	writeInfoToFile("replicas.txt", dbReplicaCount)
+
+	var redisNodes []string
+	for shardNo := 0; shardNo < dbMasterCount; shardNo++ {
+		shardName := fmt.Sprintf("%s-shard%d", offShootName, shardNo)
+
+		for podNo := 0; podNo <= dbReplicaCount; podNo++ {
+			podName := fmt.Sprintf("%s-%d", shardName, podNo)
+			dnsName := podName + "." + dbGoverningServiceName
+			redisNodes = append(redisNodes, dnsName)
+		}
+	}
+	writePodDNSToFile("redis-nodes.txt", redisNodes)
+
 }
